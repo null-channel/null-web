3372560b6004165402c5fc15eeb4a1de7d82b5c5
marek <countsmarek@gmail.com>
2022-10-03 00:46:36 -0400

    made the footer a footer instead of mid page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from '../components/navbar'
+import styles from '../styles/main.module.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
 
-  return <div>
+  return <div className={styles.main}>
             <Header/>
             <Component {...pageProps} />
           </div>
